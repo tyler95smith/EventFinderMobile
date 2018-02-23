8de7863e560f00e181777ae15c09b0304521d148
nichogenius <nichogenius@gmail.com>
2018-02-23 21:04:58 +0000

    Separated Database Credentials into secret_settings.py Added secret_settings.py to .gitignore Created secret_settings.py.sample as a template Setup our Database credentials in secret_settings.py settings.py imports our database settings

--- EventFinderServer/EventFinder/settings.py
+++ EventFinderServer/EventFinder/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = 'EventFinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# You will find no database information here.  Look in secret_settings.py
+from secret_settings import *
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
