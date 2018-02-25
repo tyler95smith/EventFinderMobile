2cc07b1b45747aa0f96aa35c0c377cb7f3b3e855
Claire <redreadergirl@hotmail.com>
2018-02-24 20:39:54 -0700

    basic edits

--- app/src/main/java/com/eventfinder/www/eventfindermobile/NewAccount.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/NewAccount.java
@@ -1,8 +1,13 @@
 package com.eventfinder.www.eventfindermobile;
 
-/**
- * Created by redre on 2/24/2018.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class NewAccount {
+public class NewAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
 }
