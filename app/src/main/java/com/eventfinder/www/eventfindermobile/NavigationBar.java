beb0b7d2d58799d87352a10649090acd53b6659f
Claire <redreadergirl@hotmail.com>
2018-02-25 01:59:15 -0700

    add navigation bar

---
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/NavigationBar.java
@@ -0,0 +1,13 @@
+package com.eventfinder.www.eventfindermobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_bar);
+    }
+}
