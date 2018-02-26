7e0fd95d368a8314e0e808299b8cf2ec7ceee1d7
Taylor Tyger <taylortyger@gmail.com>
2018-02-26 04:44:34 -0700

    Basic implementation of Notifications Activity/layout. Still need to set up viewpager so tabs can be populated properly. Warnings should also be resolved in activity_notifications.xml

--- app/src/main/java/com/eventfinder/www/eventfindermobile/NotificationsActivity.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/NotificationsActivity.java
@@ -2,6 +2,8 @@ package com.eventfinder.www.eventfindermobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+        TabLayout tabs = findViewById(R.id.notification_tabs);
+
+
+        //TO DO: Set up tabs with view pager so respective view can be displayed
     }
 }
