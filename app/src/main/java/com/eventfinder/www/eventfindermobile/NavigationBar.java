c286c5255604aff9d4662199cb93be6a7591c8ec
Claire <redreadergirl@hotmail.com>
2018-02-27 19:13:13 -0700

    remove folder accidentily added

--- app/src/main/java/com/eventfinder/www/eventfindermobile/NavigationBar.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/NavigationBar.java
@@ -1,7 +1,11 @@
 package com.eventfinder.www.eventfindermobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class NavigationBar extends AppCompatActivity {
 
@@ -9,5 +13,46 @@ public class NavigationBar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_bar);
+
+        ImageButton homebtn = (ImageButton)findViewById(R.id.home);
+        ImageButton profilebtn = (ImageButton)findViewById(R.id.profile);
+        ImageButton addbtn = (ImageButton)findViewById(R.id.add);
+        ImageButton notbtn = (ImageButton)findViewById(R.id.notification);
+        ImageButton favbtn = (ImageButton)findViewById(R.id.favorite);
+
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NavigationBar.this, HomeScreenActivity.class));
+            }
+        });
+
+        profilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NavigationBar.this, Profile.class));
+            }
+        });
+
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NavigationBar.this, AddEventActivity.class));
+            }
+        });
+
+        notbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NavigationBar.this, NotificationsActivity.class));
+            }
+        });
+
+        favbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NavigationBar.this, FavoriteEventsActivity.class));
+            }
+        });
     }
 }
