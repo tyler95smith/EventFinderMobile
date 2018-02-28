9fd12c2983eb18d01a88326a7a4842034c2dd76b
Claire <redreadergirl@hotmail.com>
2018-02-27 20:20:34 -0700

    add some navigation

--- app/src/main/java/com/eventfinder/www/eventfindermobile/HomeScreenActivity.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/HomeScreenActivity.java
@@ -1,7 +1,10 @@
 package com.eventfinder.www.eventfindermobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        ImageButton homebtn = (ImageButton)findViewById(R.id.home);
+        ImageButton profilebtn = (ImageButton)findViewById(R.id.profile);
+        ImageButton addbtn = (ImageButton)findViewById(R.id.add);
+        ImageButton notbtn = (ImageButton)findViewById(R.id.notification);
+        ImageButton favbtn = (ImageButton)findViewById(R.id.favorite);
+
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreenActivity.this, HomeScreenActivity.class));
+            }
+        });
+
+        profilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreenActivity.this, Profile.class));
+            }
+        });
+
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreenActivity.this, AddEventActivity.class));
+            }
+        });
+
+        notbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreenActivity.this, NotificationsActivity.class));
+            }
+        });
+
+        favbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreenActivity.this, FavoriteEventsActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/eventfinder/www/eventfindermobile/Login.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/Login.java
@@ -1,7 +1,10 @@
 package com.eventfinder.www.eventfindermobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button submitbtn = (Button)findViewById(R.id.submit);
+        Button newbtn = (Button)findViewById(R.id.newaccount);
+
+        submitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, HomeScreenActivity.class));
+            }
+        });
+
+        newbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, NewAccount.class));
+            }
+        });
     }
 }
