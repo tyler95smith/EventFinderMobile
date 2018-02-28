ad1bc762ff65f289c7a732edfee0617a6cda0adc
Tyler Smith <tyler95smith@gmail.com>
2018-02-27 22:07:17 -0700

    Copied a lot of code used in the notifications activity to allow switching of fragments in the fav events page. Is currently loading and switching between the two notifications fragments still need to create the past and future event fragments

--- app/src/main/java/com/eventfinder/www/eventfindermobile/FavoriteEventsActivity.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/FavoriteEventsActivity.java
@@ -1,13 +1,43 @@
 package com.eventfinder.www.eventfindermobile;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FavoriteEventsActivity extends AppCompatActivity {
+    private TabLayout tabs;
+    private ViewPager viewPager;
+    private FavoriteEventsPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_events);
+
+        //
+        // Set up tabs (TabLayout) with the Viewpager using the NotificationsPagerAdapter
+        // so the correct fragment can be displayed within the viewpager
+        tabs = (TabLayout) findViewById(R.id.fav_event_tablayout);
+        viewPager = (ViewPager) findViewById(R.id.fav_event_viewpager);
+        adapter = new FavoriteEventsPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        //
+        //Update current tab item based on tab position
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                viewPager.setCurrentItem(position);
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/FavoriteEventsPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.eventfinder.www.eventfindermobile;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+//===================================================================
+//
+//  FragmentPagerAdapter subclass for use with the viewpager
+//  used to determine which Fragment to display based on tabbed
+//  position (notifications fragment/messages fragment).
+//
+//===================================================================
+public class FavoriteEventsPagerAdapter extends FragmentPagerAdapter{
+    private static final int TAB_COUNT = 2;
+    private static final int NOTIFICATIONS_TAB_POS = 0;
+    //Do not need MESSAGES_TAB_POS with current logic
+
+    //---------------------------------------------------------------
+    //  Constructor
+    //---------------------------------------------------------------
+    public FavoriteEventsPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    //---------------------------------------------------------------
+    //  Determines the corresponding fragment for each position(tab)
+    //---------------------------------------------------------------
+    @Override
+    public Fragment getItem(int position) {
+        if(position == NOTIFICATIONS_TAB_POS) {
+            return new PastEventsListFragment();
+        } else {
+            return new FutureEventsListFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return(TAB_COUNT);
+    }
+
+}
