e1fcda41b994b053570acb09ce3504d4d20a2305
Claire <redreadergirl@hotmail.com>
2018-02-27 22:13:18 -0700

    add some more navigation

--- app/src/main/java/com/eventfinder/www/eventfindermobile/NewAccount.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/NewAccount.java
@@ -1,7 +1,10 @@
 package com.eventfinder.www.eventfindermobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewAccount extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class NewAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        Button accountbtn = (Button)findViewById(R.id.account);
+
+        accountbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewAccount.this, HomeScreenActivity.class));
+            }
+        });
     }
 }
