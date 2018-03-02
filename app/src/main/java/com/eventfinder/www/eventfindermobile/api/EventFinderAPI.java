6fc49888d53de2977c2bcace384292b6b6fdcc91
Taylor Tyger <taylortyger@gmail.com>
2018-03-01 21:41:37 -0700

    Created api package

---
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/api/EventFinderAPI.java
@@ -0,0 +1,17 @@
+package com.eventfinder.www.eventfindermobile.api;
+
+/**
+ * Created by Taylor on 3/1/2018.
+ */
+public class EventFinderAPI {
+    public final static String API_URL = "";
+
+    //Not sure exactly how this will be implemented and what it will return, but the
+    // basic idea is then we can put all of the logic and make all the requests
+    // from here, simplifying our lives and abstracting it away from the logic within
+    // the Activities and such.
+    public String createUser()
+    {
+        return "Something";
+    }
+}
