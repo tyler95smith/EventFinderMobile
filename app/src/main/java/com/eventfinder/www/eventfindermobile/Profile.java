fca7bd6b2d156835d85df4bef60ba7ee07c39cfc
Claire <redreadergirl@hotmail.com>
2018-03-02 00:26:16 -0700

    add internet permission

--- app/src/main/java/com/eventfinder/www/eventfindermobile/Profile.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/Profile.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        String url = "https://api.nthbox.com/api";
 
         ImageButton homebtn = (ImageButton)findViewById(R.id.home);
         ImageButton profilebtn = (ImageButton)findViewById(R.id.profile);
@@ -53,5 +57,43 @@ public class Profile extends AppCompatActivity {
                 startActivity(new Intent(Profile.this, FavoriteEventsActivity.class));
             }
         });
+
+        final Button edit = (Button)findViewById(R.id.editButton);
+        final EditText name = (EditText)findViewById(R.id.NameBox);
+        final EditText username = (EditText)findViewById(R.id.UsernameBox);
+        final EditText email = (EditText)findViewById(R.id.EmailBox);
+        final Button addInt = (Button)findViewById(R.id.addInterests);
+
+        addInt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open addInterest panel
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edit.getText() == "Edit Profile") {
+                    name.setEnabled(true);
+                    username.setEnabled(true);
+                    email.setEnabled(true);
+                    addInt.setVisibility(View.VISIBLE);
+                    edit.setText("Submit");
+                } else {
+                    edit.setText("Edit Profile");
+                    username.setEnabled(false);
+                    email.setEnabled(false);
+                    addInt.setVisibility(View.GONE);
+                    name.setEnabled(false);
+                }
+            }
+        });
+
+
+
+
+
+
     }
 }
