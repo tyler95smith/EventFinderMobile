82efaeb6ef83375cd53d6caddb75d7f3bc2a6543
Claire <redreadergirl@hotmail.com>
2018-03-18 23:22:23 -0600

    fix double click of edit button

--- app/src/main/java/com/eventfinder/www/eventfindermobile/Profile.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/Profile.java
@@ -4,11 +4,15 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 
 public class Profile extends AppCompatActivity {
 
@@ -23,6 +27,11 @@ public class Profile extends AppCompatActivity {
         ImageButton addbtn = (ImageButton)findViewById(R.id.add);
         ImageButton notbtn = (ImageButton)findViewById(R.id.notification);
         ImageButton favbtn = (ImageButton)findViewById(R.id.favorite);
+        final Button edit = (Button)findViewById(R.id.editButton);
+        final EditText name = (EditText)findViewById(R.id.NameBox);
+        final EditText username = (EditText)findViewById(R.id.UsernameBox);
+        final EditText email = (EditText)findViewById(R.id.EmailBox);
+        final Button addInt = (Button)findViewById(R.id.addInterests);
 
         homebtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,11 +68,7 @@ public class Profile extends AppCompatActivity {
             }
         });
 
-        final Button edit = (Button)findViewById(R.id.editButton);
-        final EditText name = (EditText)findViewById(R.id.NameBox);
-        final EditText username = (EditText)findViewById(R.id.UsernameBox);
-        final EditText email = (EditText)findViewById(R.id.EmailBox);
-        final Button addInt = (Button)findViewById(R.id.addInterests);
+
 
         addInt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,22 +81,17 @@ public class Profile extends AppCompatActivity {
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(edit.getText() == "Edit Profile") {
+                if(!name.isEnabled()) {
+                    addInt.setVisibility(VISIBLE);
                     name.setEnabled(true);
-                    username.setEnabled(true);
-                    email.setEnabled(true);
-                    addInt.setVisibility(View.VISIBLE);
                     edit.setText("Submit");
                 } else {
-                    edit.setText("Edit Profile");
-                    username.setEnabled(false);
-                    email.setEnabled(false);
-                    addInt.setVisibility(View.GONE);
+                    addInt.setVisibility(GONE);
                     name.setEnabled(false);
+                    edit.setText("Edit Profile");
                 }
             }
         });
-
 
 
 
