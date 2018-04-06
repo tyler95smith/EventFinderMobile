c9e422b61c635d55db48fff4914d12f9439fd69d
Claire <redreadergirl@hotmail.com>
2018-04-06 10:23:21 -0600

    view event stuff

--- app/src/main/java/com/eventfinder/www/eventfindermobile/ViewEventActivity.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/ViewEventActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ViewEventActivity extends AppCompatActivity {
 
@@ -14,10 +18,22 @@ public class ViewEventActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         User user = (User)bundle.getSerializable("user");
         Event event = (Event)bundle.getSerializable("event");
+        final TextView name = (TextView)findViewById(R.id.eventName);
+        final TextView when = (TextView)findViewById(R.id.Date);
+        final TextView time = (TextView)findViewById(R.id.Time);
+        final TextView where = (TextView)findViewById(R.id.Place);
+        final MultiAutoCompleteTextView des = (MultiAutoCompleteTextView)findViewById(R.id.description);
+        final Button request = (Button)findViewById(R.id.request);
+        final Button message = (Button)findViewById(R.id.message);
 
         if(event != null)
         {
-
+            name.setText(event.eventName);
+            String date = event.eventDate.toString();
+            when.setText(date);
+            time.setText(event.time);
+            where.setText(event.location);
+            des.setText(event.description);
         }
 
         final Button report = (Button)findViewById(R.id.reportEvent);
