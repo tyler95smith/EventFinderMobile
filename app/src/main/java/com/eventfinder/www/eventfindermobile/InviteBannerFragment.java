33d47362856f7fcea1e6b4338087e6c2568ab960
Tyler Smith <tyler95smith@gmail.com>
2018-04-26 21:29:28 -0600

    modified the InviteBannerFragment to access the buttons in the gui and add on click listeners

--- app/src/main/java/com/eventfinder/www/eventfindermobile/InviteBannerFragment.java
+++ app/src/main/java/com/eventfinder/www/eventfindermobile/InviteBannerFragment.java
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -25,11 +27,32 @@ public class InviteBannerFragment extends ListFragment {
             notification
     };
 
+    private Button btnAccept;
+    private Button btnDecline;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
         List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
         Intent intent = getActivity().getIntent();
+        btnAccept = view.findViewById(R.id.btnAccept);
+        btnDecline = view.findViewById(R.id.btnDecline);
 
+        btnAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // make api call setting the rsvp to accepted
+                Toast.makeText(getActivity(), "You clicked Accept",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDecline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // make api call setting the rsvp to declined
+                Toast.makeText(getActivity(), "You clicked Decline",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         for(int i = 0; i < notifs.length; i++) {
             HashMap<String, String> hm = new HashMap<String, String>();
@@ -45,7 +68,7 @@ public class InviteBannerFragment extends ListFragment {
 
         SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.fragment_invitebanner, from, to);
         setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return view;
     }
 
     OnHeadlineSelectedListener mCallback;
